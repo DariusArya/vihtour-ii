4c8e151dd2f9e82c2ed6b68116774df8e6319afa
Eko Studio <>
2020-02-11 13:36:47 +0000

    1

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,15 @@ function onReady(player, ctx)
 {
     ////////// START YOUR CODE HERE \\\\\\\\\\
     player.variables.register('num_clicks', {initialValue: '0'});
-
+    player.variables.register('prop1',{initialValues: ''});
+    
     player.on('interactions.report', 
         function(id, data) 
         {
             console.log('interaction occurred!', id, data);
             player.variables.setValue('num_clicks', parseInt(player.variables.getValue('num_clicks')) + 1);
+            player.variables.setValue('prop1', player.ui.getProps('button_main_7b1a80'));
+           
         });
     // Visit http://developer.eko.com for detailed API documentation
     //////////  END YOUR CODE HERE  \\\\\\\\\\
